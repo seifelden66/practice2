ae4050b13c035993c7eb2f0880001ead2833338c
seifelden66 <seifelden66@gmail.com>
2023-11-10 01:29:38 +0200

    webside modified

--- app.vue
+++ app.vue
@@ -4,13 +4,21 @@
       <Nav />
       <Hero />
       <div class="md:absolute md:left-[50%] md:translate-x-[-50%] md:top-[120vh]">
-        <TopCities />
-        <TopRest />
-        <offers />
-        <download />
+        <div data-aos="fade-up" :data-aos-duration="450" :data-aos-delay="200">
+          <TopCities />
+        </div>
+        <div data-aos="fade-up" :data-aos-duration="450" :data-aos-delay="200">
+          <TopRest />
+        </div>
+        <div data-aos="fade-up" :data-aos-duration="450" :data-aos-delay="200">
+          <offers />
+        </div>
+        <div data-aos="fade-up" :data-aos-duration="450" :data-aos-delay="200">
+          <download />
+        </div>
       </div>
     </div>
-    <div class="md:absolute bottom-0 -z-40 w-full ">
+    <div class="md:absolute bottom-0 -z-40 w-full">
       <Footer />
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  plugins: [{ src: '~/plugins/aos.ts', mode: 'client' },],
   css: [
     "/assets/main.scss"
   ],
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss','nuxt-swiper']
 })
