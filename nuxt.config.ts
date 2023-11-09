d86d6bc8838ddb00b716a993baf5bf6546006358
seifelden66 <seifelden66@gmail.com>
2023-11-10 01:55:58 +0200

    webside modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  plugins: [{ src: '~/plugins/aos.ts', mode: 'client' },],
+  
   css: [
     "/assets/main.scss"
   ],
